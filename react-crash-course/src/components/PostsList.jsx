cefadb7fff861baa90661d5b0a960caed620071d
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-07-24 12:23:44 +0200

    add conditional state

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -2,8 +2,10 @@ import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 import { useState } from "react"; // Importing useState hook to manage state in functional components
+import Modal from "./Modal"; // Importing Modal component to display a modal dialog
 
 function PostsList() {
+  const [isModalOpen, setIsModalOpen] = useState(true); // This is a hook that allows you to add state to a functional component
   const [enteredBody, setenteredBody] = useState(""); // This is a hook that allows you to add state to a functional component
   const [enteredAuth, setenteredAuth] = useState("");
 
@@ -14,12 +16,31 @@ function PostsList() {
   function changeAuthHandeler(event) {
     setenteredAuth(event.target.value);
   }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
   return (
     <>
-      <NewPost
-        changeBodyHandeler={changeBodyHandeler}
-        changeAuthHandeler={changeAuthHandeler}
-      />
+      {/* {isModalOpen ? (
+        <Modal onClose={closeModal}>
+          <NewPost
+            changeBodyHandeler={changeBodyHandeler}
+            changeAuthHandeler={changeAuthHandeler}
+          />
+        </Modal>
+      ) : (
+        false
+      )} */}
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          <NewPost
+            changeBodyHandeler={changeBodyHandeler}
+            changeAuthHandeler={changeAuthHandeler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post auth={enteredAuth} body={enteredBody} />
         <Post auth="Ali" body="Bye" />
