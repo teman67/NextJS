0cfda159f46c4770ccf7a1e9a1ee7a9aa46f69e7
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-07-25 15:08:39 +0200

    add submit button works

--- react-crash-course/src/components/NewPost.jsx
+++ react-crash-course/src/components/NewPost.jsx
@@ -1,8 +1,30 @@
 import classes from "./NewPost.module.css";
+import { useState } from "react"; // Importing useState hook to manage state in functional components
 
-function NewPost({ changeBodyHandeler, changeAuthHandeler, onClose }) {
+function NewPost({ changeBodyHandeler, changeAuthHandeler, onClose, addPost }) {
+  const [enteredBody, setenteredBody] = useState(""); // This is a hook that allows you to add state to a functional component
+  const [enteredAuth, setenteredAuth] = useState("");
+
+  function changeBodyHandeler(event) {
+    setenteredBody(event.target.value);
+  }
+
+  function changeAuthHandeler(event) {
+    setenteredAuth(event.target.value);
+  }
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      body: enteredBody,
+      author: enteredAuth,
+    };
+    addPost(postData); // Call the addPost function passed as a prop to add the new post
+    console.log(postData);
+    onClose(); // Call the onClose function to close the modal after submitting the form
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandeler} />
--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -5,15 +5,10 @@ import { useState } from "react"; // Importing useState hook to manage state in 
 import Modal from "./Modal"; // Importing Modal component to display a modal dialog
 
 function PostsList({ isCloseModal, onStopPoting }) {
-  const [enteredBody, setenteredBody] = useState(""); // This is a hook that allows you to add state to a functional component
-  const [enteredAuth, setenteredAuth] = useState("");
+  const [posts, setPosts] = useState([]); // State to hold the list of posts
 
-  function changeBodyHandeler(event) {
-    setenteredBody(event.target.value);
-  }
-
-  function changeAuthHandeler(event) {
-    setenteredAuth(event.target.value);
+  function addPostHandler(postData) {
+    setPosts((prevPosts) => [...prevPosts, postData]); // Update the posts
   }
 
   return (
@@ -30,17 +25,25 @@ function PostsList({ isCloseModal, onStopPoting }) {
       )} */}
       {isCloseModal && (
         <Modal onClose={onStopPoting}>
-          <NewPost
-            changeBodyHandeler={changeBodyHandeler}
-            changeAuthHandeler={changeAuthHandeler}
-            onClose={onStopPoting}
-          />
+          <NewPost onClose={onStopPoting} addPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post auth={enteredAuth} body={enteredBody} />
-        <Post auth="Ali" body="Bye" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post, index) => {
+            console.log("Rendering post at index:", index);
+            return <Post key={index} auth={post.author} body={post.body} />;
+          })}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <>
+          <h2>Add some posts!</h2>
+          <p style={{ textAlign: "center", color: "white" }}>
+            No posts available.
+          </p>
+        </>
+      )}
     </>
   );
 }
