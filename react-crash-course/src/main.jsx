563b6e095afd120845f0bf2c11530a5f407d1d70
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-07-28 16:33:27 +0200

    add react router

--- react-crash-course/src/main.jsx
+++ react-crash-course/src/main.jsx
@@ -3,10 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Post from "./components/Post.jsx";
+import ReactDom from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import NewPost from "./components/NewPost.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  { path: "/create-post", element: <NewPost /> },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
+    {/* Uncomment the line below to render the Post component */}
     {/* <Post /> */}
   </StrictMode>
 );
