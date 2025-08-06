06102b4c7328a363dbbc2576641fc84aca5ae706
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-08-06 16:49:27 +0200

    test use client

--- 05-onwards-foodies-starting-project/components/main-header.js
+++ 05-onwards-foodies-starting-project/components/main-header.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import Image from "next/image";
 
 export default function MainHeader() {
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
-        <img src={logoImg.src} alt="Foodies Logo" />
+        <Image src={logoImg} alt="Foodies Logo" />
         NextLevel Food
       </Link>
 
