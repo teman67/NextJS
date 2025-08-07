e22cf7b72d9fbee36ee7fc9a05fedbc9a4e309cf
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-08-07 12:18:57 +0200

    add error measge function

---
+++ 05-onwards-foodies-starting-project/app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ErrorPage() {
+  return (
+    <main className="error">
+      <h1>Something went wrong!</h1>
+      <p>We couldn't load the meals at this time. Please try again later.</p>
+    </main>
+  );
+}
--- 05-onwards-foodies-starting-project/lib/meals_db.js
+++ 05-onwards-foodies-starting-project/lib/meals_db.js
@@ -4,6 +4,7 @@ const db = sql("meals.db");
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 4000));
+  //   throw new Error("Database connection failed"); // Simulating a database error
   const meals = db.prepare("SELECT * FROM meals").all();
   return meals;
 }
