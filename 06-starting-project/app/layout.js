ef9a21c8d6a504bd0ef5b9bf4e3b6359c7fe4e6a
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-08-23 01:07:49 +0200

    fix screen size issue

--- 06-starting-project/app/layout.js
+++ 06-starting-project/app/layout.js
@@ -3,11 +3,15 @@ import "./globals.css";
 export const metadata = {
   title: "Next.js Page Routing & Rendering",
   description: "Learn how to route to different pages.",
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>{children}</body>
     </html>
   );
