1ade779c9ebae9057f4fa1081a022fd03884d3cc
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-08-27 16:17:42 +0200

    add dynamic metadata

--- 09-optimization-images-metadata/app/feed/page.js
+++ 09-optimization-images-metadata/app/feed/page.js
@@ -1,5 +1,18 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// export const metadata = {
+//   title: "Browse all our X posts",
+//   description: "A feed of all posts by all users.",
+// };
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  return {
+    title: `Browse all our ${posts.length} posts`,
+    description: "A feed of all posts by all users.",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
