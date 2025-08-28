6b1aea97e0a93f3904719934797a2531df7acc9d
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-08-28 11:11:19 +0200

    add error for invalid signup

---
+++ 10-authentication/actions/auth-actions.js
@@ -0,0 +1,19 @@
+"use server";
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Email must be valid.";
+  }
+  if (password.trim().length < 8) {
+    errors.password = "Password must be at least 8 characters long.";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors: errors };
+  }
+}
